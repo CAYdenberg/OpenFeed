ce19f1b0bd861fb697eac4e146199e98faf4699a
@CAYdenberg <ydenberg@gmail.com>
2019-01-16 14:48:54 -0700

    Update to koala-server 0..0.3

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const rss2json = require('rss-to-json-feed')
 
 Koala(router => {
   router.get('/convert', (req, res) => {
-    console.log('CONVERT')
     rss2json.parseURL(req.query.url, {}, (err, json) => {
       if (err) return res.status(400).json({error: 'Feed could not be parsed'})
       return res.json(json)
