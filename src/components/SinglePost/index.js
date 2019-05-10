0244507c59d48e25c32158b536d56ea5cea5fcf9
@CAYdenberg <ydenberg@gmail.com>
2019-05-10 16:12:52 -0600

    Use HTML when available

--- src/components/SinglePost/index.js
+++ src/components/SinglePost/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import DOMPurify from 'dompurify'
 
 const SinglePost = ({post}) => {
   return (
     <article className="content">
       <h1>{post.title}</h1>
-      <div>
-        {post.content_html}
-      </div>
+      <div dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(post.content_html)
+      }} />
     </article>
   )
 }
