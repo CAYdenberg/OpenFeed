4330fa0dee165687f56c2c704f198832c6fb23e2
@CAYdenberg <ydenberg@gmail.com>
2019-06-21 16:40:55 -0600

    Open posts in place

--- src/components/Timeline/Post.js
+++ src/components/Timeline/Post.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {DateTime} from 'luxon'
+import DOMPurify from 'dompurify'
 import IsReadSwitch from '../IsReadSwitch'
 import {getId} from '../../helpers'
 import Icon, {openOriginal} from '../Icons'
 
 const Post = (props) => {
-  const {feed, title, summary, markRead} = props
+  const {feed, title, summary, markRead, isOpen, content_html} = props
   const date = props.date_published ? DateTime.fromISO(props.date_published) : null
 
   return (
     <div className="card card--post">
-      <a href="#" className="card-header" onClick={() => props.openPost(props._id)}>
+      <a href="#" className="card-header" onClick={(e) => props.openPost(e, props._id)}>
         <h3 className="card-header-title">
           {title}
         </h3>
@@ -19,7 +20,14 @@ const Post = (props) => {
       </a>
 
       <div className="card-content">
-        <p className="card__summary">{summary}</p>
+        {isOpen ? (
+          <div dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(content_html)
+          }} />
+        ) : (
+          <p className="card__summary">{summary}</p>
+        )}
+
         <div className="is-size-7 has-text-grey metadata">
           <time dateTime={props.date_published} className="metadata__item">
             {date.toLocaleString()}
@@ -48,7 +56,8 @@ Post.propTypes = {
   feed: PropTypes.shape({
     title: PropTypes.string.isRequired,
     author: PropTypes.string
-  })
+  }),
+  isOpen: PropTypes.bool,
 }
 
 export default Post
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-import {timelinePosts} from '../../store/selectors'
+import {timelinePosts, openPost} from '../../store/selectors'
 import {actions} from '../../store/posts'
 import Post from './Post'
 
@@ -10,22 +10,29 @@ const mapStateToProps = state => {
   return {
     posts: timelinePosts(state),
     loadState: state.posts.loadState,
+    openPost: openPost(state)
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    openPost: (_id) => {
+    markRead: (e, _id) => {
+      e.preventDefault()
+      dispatch(actions.markRead(_id))
+    },
+    handleOpenPost: (e, _id) => {
+      e.preventDefault()
       dispatch(actions.openPost(_id))
       dispatch(actions.markRead(_id))
     },
-    markRead: (_id) => {
-      dispatch(actions.markRead(_id))
+    handleClosePost: (e) => {
+      e.preventDefault()
+      dispatch(actions.closePost())
     }
   }
 }
 
-const Timeline = ({loadState, posts, openPost, markRead}) => {
+const Timeline = ({loadState, posts, markRead, openPost, handleOpenPost}) => {
   if (loadState === 1) {
     return <div className="loading" />
   }
@@ -36,7 +43,13 @@ const Timeline = ({loadState, posts, openPost, markRead}) => {
 
   return (
     (posts.map(post =>
-      <Post {...post} openPost={openPost} markRead={markRead} key={post._id} />
+      <Post
+        {...post}
+        openPost={handleOpenPost}
+        isOpen={openPost && (openPost._id === post._id)}
+        markRead={markRead}
+        key={post._id}
+      />
     ))
   )
 }
